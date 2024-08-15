67c643d78321ef18e003a70d406fae2d25aefa55
Thiago dos Passos Lima <166858263+Thichi99@users.noreply.github.com>
2024-08-15 18:34:54 -0300

    Alteracao do Service.ts para o Deploy

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-ica4.onrender.com/'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
